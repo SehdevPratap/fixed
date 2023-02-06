ca83659d72561af5094d7f09c72cdb0006841c2a
Sehdev <pratapsehdev@gmail.com>
2023-02-06 15:43:43 +0530

    Clash commit

---
+++ clash.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class m
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your clash of clan code:");
+        int n=Sc.nextInt();
+        System.out.println("Your phone number");
+        int pno=sc.nextInt();
+    }
+}
